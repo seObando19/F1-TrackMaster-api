b18e7380b51048a23922a963e0693d629ed00e7b
Sebastian Obando <sebastiano-10@hotmail.com>
2023-07-14 10:14:24 -0500

    [FEAT] added controller for circuit schema

--- src/modules/circuit/circuit.controller.ts
+++ src/modules/circuit/circuit.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { CircuitService } from './circuit.service';
 
 @Controller('circuit')
-export class CircuitController {}
+export class CircuitController {
+
+  constructor(private circuitService: CircuitService){}
+
+  @Get()
+  async getllCircuits(){
+    return this.circuitService.getAllCircuits();
+  }
+
+  @Get(':id')
+  async getCircuitById(@Param('id') id: string){
+    return this.circuitService.getCircuitById(id);
+  }
+
+  @Post()
+  async createCircuit(@Body() payload: any){
+    return this.circuitService.createCircuit(payload);
+  }
+
+  @Put(':id')
+  async updateCircuit(@Param('id') id: string, @Body() payload: any) {
+    return this.circuitService.updateCircuit(id, payload);
+  }
+
+  @Delete(':id')
+  async deleteCircuit(@Param(':id') id: string) {
+    return this.circuitService.deleteCircuit(id);
+  }
+
+}
